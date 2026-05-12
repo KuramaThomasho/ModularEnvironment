84f45c85f055e224468591cb554bad473115417e
Kursma <thomashoho11@gmail.com>
2026-05-12 14:23:27 +0200

    fixed chair spawn
    spawning thing works with quads, will begin check with other objects

--- Assets/Scripts/ObjectLibrary.cs
+++ Assets/Scripts/ObjectLibrary.cs
@@ -16,6 +16,7 @@ public class ObjectLibrary : MonoBehaviour
     {
         m_ARPlane = GetComponent<ARPlane>();
         m_PlaneMeshRenderer = GetComponent<MeshRenderer>();
+        Debug.Log("Got the plane and mesh renderer");
     }
 
     void Start()
@@ -33,24 +34,31 @@ public class ObjectLibrary : MonoBehaviour
         {
             case PlaneClassifications.Floor:
                 planeMatColor = Color.green;
+                Debug.Log("Its a Floor");
                 break;
             case PlaneClassifications.WallFace:
                 planeMatColor = Color.red;
+                Debug.Log("Its a wall");
                 break;
             case PlaneClassifications.Ceiling:
                 planeMatColor = Color.blue;
+                Debug.Log("Its a ceiling");
                 break;
             case PlaneClassifications.Table:
                 planeMatColor = Color.yellow;
+                Debug.Log("Its a Table");
                 break;
             case PlaneClassifications.Seat:
                 planeMatColor = Color.magenta;
+                Debug.Log("Its a Seat");
                 break;
             case PlaneClassifications.DoorFrame:
                 planeMatColor = Color.cyan;
+                Debug.Log("Its a Door");
                 break;
             case PlaneClassifications.WindowFrame:
                 planeMatColor = Color.white;
+                Debug.Log("Its a Window");
                 break;
         }
 
--- Assets/Scripts/QRCodeManager.cs
+++ Assets/Scripts/QRCodeManager.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
 using Meta.XR.MRUtilityKit;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine.Rendering;
+using Unity.VisualScripting;
 
 
 public class QRCodeManager : MonoBehaviour
 {
     public GameObject debugObject;
     public GameObject Chair;
+    public List<GameObject> furnitureList;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         MRUK.Instance.SceneSettings.TrackableAdded.AddListener(OnQRCodeTracked);
+        MRUK.Instance.SceneSettings.TrackableAdded.AddListener(OnChairQRCodeTracked);
     }
 
     public void OnQRCodeTracked(MRUKTrackable qrCode)
@@ -20,39 +26,44 @@ public class QRCodeManager : MonoBehaviour
             Debug.Log("QR not correct");
             return;
         }
-
         string qrURL = qrCode.MarkerPayloadString;
-        Debug.Log("QR code tracked with URL: " + qrURL);
-
-        Vector3 targetPosition = qrCode.transform.position;
-        Quaternion targetRotation = Quaternion.LookRotation(-qrCode.transform.forward, qrCode.transform.up);
-
-        float width = qrCode.PlaneRect.Value.width;
-        float height = qrCode.PlaneRect.Value.height;
-
-        if (qrURL.Contains("chair"))
+        if (qrURL.Contains("bgn"))
         {
-            Debug.Log("Its a chair!");
-            GameObject spawnedChair = Instantiate(Chair, targetPosition, targetRotation);      
-
-            Vector3 targetScale = new Vector3(width, height, 0);
-
-            spawnedChair.transform.localScale = targetScale;
-            spawnedChair.transform.parent = qrCode.transform;
-        }
-        else
-        {
-            GameObject spawned = Instantiate(debugObject, targetPosition, targetRotation);
-
-            Vector3 targetScale = new Vector3(width, height, 0);
-
-            spawned.transform.localScale = targetScale;
-            spawned.transform.parent = qrCode.transform;
-
-            Debug.Log("Object spawned at QR code position with scale: " + targetScale);
+            QRObjectSpawner(qrCode, debugObject);
+            Debug.Log("Object spawned at QR code");
         }
         
     }
 
+    public void OnChairQRCodeTracked(MRUKTrackable qrCode)
+    {
+        if (qrCode.TrackableType != OVRAnchor.TrackableType.QRCode)
+        {
+            Debug.Log("QR not correct");
+            return;
+        }
 
+        string qrURL = qrCode.MarkerPayloadString;
+        Debug.Log("QR code tracked with URL: " + qrURL);
+        if (qrURL.Contains("chair"))
+        {
+            Debug.Log("Its a chair!");
+            QRObjectSpawner(qrCode, Chair);
+        }
+    }
+
+    private void QRObjectSpawner(MRUKTrackable qrCode, GameObject prefab)
+    {
+        Vector3 targetPosition = qrCode.transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(-qrCode.transform.forward, qrCode.transform.up);
+
+        GameObject spawned = Instantiate(prefab, targetPosition, targetRotation);
+
+        float width = qrCode.PlaneRect.Value.width;
+        float height = qrCode.PlaneRect.Value.height;
+        Vector3 targetScale = new Vector3(width, height, 0);
+
+        spawned.transform.localScale = targetScale;
+        spawned.transform.parent = qrCode.transform;
+    }
 }
